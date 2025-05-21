6aa65155232b30c17c01e683e1dec2d946106b44
Jhos10 <jhostinposada7@gmail.com>
2025-05-21 11:29:25 -0500

    Se realizo el modelo para la compra

--- compra/models.py
+++ compra/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from usuarios.models import *
+from producto.models import *
 # Create your models here.
+class Compra(models.Model):
+    id_producto = models.ForeignKey(Producto, related_name='producto')
+    id_user = models.ForeignKey(User, related_name='usuario')
+    cantidad = models.IntegerField()
+    fecha = models.DateTimeField()
+    def __str__(self):
+        return self.pk
+    
