197dd9219e441c603acb0dcadb5113489fc70e76
Jhos10 <jhostinposada7@gmail.com>
2025-05-21 11:32:59 -0500

    Se hicieron las migraciones del nuevo modelo de compra

--- compra/models.py
+++ compra/models.py
@@ -3,8 +3,8 @@ from usuarios.models import *
 from producto.models import *
 # Create your models here.
 class Compra(models.Model):
-    id_producto = models.ForeignKey(Producto, related_name='producto')
-    id_user = models.ForeignKey(User, related_name='usuario')
+    id_producto = models.ForeignKey(Producto, related_name='producto', on_delete=models.CASCADE)
+    id_user = models.ForeignKey(User, related_name='usuario', on_delete=models.CASCADE)
     cantidad = models.IntegerField()
     fecha = models.DateTimeField()
     def __str__(self):
