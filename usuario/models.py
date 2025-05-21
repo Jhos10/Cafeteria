7c1663ffd8c2b1e4f00610983deda85bfcb7c202
Jhos10 <jhostinposada7@gmail.com>
2025-05-20 22:33:55 -0500

    Se hizo la tabla del usuairo utilizando el modelo de django que esta en la carpeta usuario

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=80)
+    apellidos = models.CharField(max_length=80)
+    es_admin = models.BooleanField(default=False)
+    correo = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=80, unique=True)
