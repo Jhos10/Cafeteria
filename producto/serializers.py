53cfb051d2b0628f5d92367fa56e8ea2bf0594b4
Jhos10 <jhostinposada7@gmail.com>
2025-05-21 11:47:13 -0500

    Se hizo el serializer para el producto y tambien para el productoCategoria

---
+++ producto/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Producto, Producto_cateogoria
+
+class ProductoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = "__all__"
+
+class ProductoCategoriaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Producto_cateogoria
+        fields = "__all__"
+        
